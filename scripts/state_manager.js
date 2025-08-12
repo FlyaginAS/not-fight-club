bff31bceaec43e0dde48834974c1339d231cd254
FlyaginAS <rus.reg.36@gmail.com>
2025-08-12 14:17:22 +0600

    feat: add basic markup for character page

--- scripts/state_manager.js
+++ scripts/state_manager.js
@@ -14,3 +14,43 @@ export function loadState() {
 export function resetState() {
   localStorage.setItem("appState", JSON.stringify({}));
 }
+// //!app state structure
+// appState = {
+//   hero: {
+//     name: "Hero",
+//     avatar: 0,
+//     health: 100,
+//     wins: 0,
+//     loses: 0,
+//     body: {
+//       head: {
+//         protected: false,
+//       },
+//       neck: {
+//         protected: false,
+//       },
+//       body: {
+//         protected: false,
+//       },
+//       belly: {
+//         protected: false,
+//       },
+//       legs: {
+//         protected: false,
+//       },
+//     },
+//     attackZones: ["head"],
+//     isCriticalAttack: false,
+//     damage: 10,
+//     critDamage: 30,
+//     isCritDamage: Math.random() * 10 < 2,
+//   },
+//   enemy: {
+//     name: "Enemy",
+//     avatar: 0,
+//     health: 100,
+//   },
+//   heroes: [{}],
+//   enemies: [{}],
+//   log: ["strs"],
+// };
