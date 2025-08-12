b8af0cc7cd8f9dbb98452d041d11ea15ef7b4eb2
FlyaginAS <rus.reg.36@gmail.com>
2025-08-12 10:57:56 +0600

    feat: create basic markup for pages

--- scripts/registry.js
+++ scripts/registry.js
@@ -1,13 +1,19 @@
 import { saveState, loadState, resetState } from "./state_manager.js";
 
+const modalRegistry = document.querySelector(".modal-registry");
 const registryForm = document.querySelector(".registry");
 const name = document.querySelector(".registry__input");
 const submit = document.querySelector(".registry__submit");
 
-registryForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  evt.stopPropagation();
-  resetState();
-  saveState({ characterName: registryForm.character_name.value });
-  registryForm.classList.add("hidden");
-});
+const appState = loadState();
+console.log(appState);
+
+if (!appState.characterName) {
+  registryForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    resetState();
+    saveState({ characterName: registryForm.character_name.value });
+    modalRegistry.classList.add("hidden");
+  });
+}
