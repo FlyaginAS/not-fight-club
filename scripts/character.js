42f93126870a5d3d905e23e320583567f3a52e09
FlyaginAS <rus.reg.36@gmail.com>
2025-08-13 13:32:40 +0600

    feat: implement change and save character avatar

--- scripts/character.js
+++ scripts/character.js
@@ -1,4 +1,4 @@
-import { loadState } from "./state_manager.js";
+import { loadState, saveState } from "./state_manager.js";
 
 function loadHeroName() {
   const characterNameEl = document.querySelector(".character-info__name");
@@ -24,5 +24,32 @@ function initPage() {
   loadHeroAvatar();
   loadHeroName();
   loadHeroStats();
+
+  addListenerOnAvatarsContainer();
 }
 initPage();
+//*change hero avatar from a library*************************************** */
+function changeHeroAvatar(url) {
+  //change avatar on page
+  const characterImgEl = document.querySelector(".character-info__img");
+  characterImgEl.src = url;
+  //Save state
+  const regex = /(\d+)\.png$/;
+  const match = url.match(regex);
+
+  if (match) {
+    const imageNumber = match[1];
+    const state = loadState();
+    state.hero.avatar = imageNumber;
+    saveState(state);
+  }
+}
+
+function addListenerOnAvatarsContainer() {
+  const avatarsContainer = document.querySelector(".character-avatars");
+  avatarsContainer.addEventListener("click", (evt) => {
+    if (evt.target.tagName === "IMG") {
+      changeHeroAvatar(evt.target.src);
+    }
+  });
+}
