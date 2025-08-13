4c14ac136de367f75c71bc6c82fccbb42ecd3f5f
FlyaginAS <rus.reg.36@gmail.com>
2025-08-13 12:10:02 +0600

    feat: add saving functionality on registry page

--- scripts/registry.js
+++ scripts/registry.js
@@ -1,21 +1,22 @@
-import { saveState, loadState, resetState } from "./state_manager.js";
+import { saveState, loadState, resetState, appState } from "./state_manager.js";
 
 const modalRegistry = document.querySelector(".modal-registry");
 const registryForm = document.querySelector(".registry");
-const name = document.querySelector(".registry__input");
-const submit = document.querySelector(".registry__submit");
+const heroName = document.querySelector(".registry__input");
+// const submit = document.querySelector(".registry__submit");
 
-const appState = loadState();
-console.log(appState);
-
-if (!appState.characterName) {
+if (!loadState()?.hero?.name) {
   modalRegistry.classList.remove("hidden");
 
   registryForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
     evt.stopPropagation();
-    resetState();
-    saveState({ characterName: registryForm.character_name.value });
+    saveState(appState);
+
+    const newState = loadState();
+    newState.hero.name = registryForm.character_name.value.trim();
+    saveState(newState);
+
     modalRegistry.classList.add("hidden");
   });
 }
--- scripts/state_manager.js
+++ scripts/state_manager.js
@@ -1,6 +1,6 @@
-export function saveState(obj) {
-  const oldState = loadState();
-  localStorage.setItem("appState", JSON.stringify({ ...oldState, ...obj }));
+export function saveState(appState) {
+  // const oldState = loadState();
+  localStorage.setItem("appState", JSON.stringify(appState));
 }
 
 export function loadState() {
@@ -15,42 +15,42 @@ export function resetState() {
   localStorage.setItem("appState", JSON.stringify({}));
 }
 // //!app state structure
-// appState = {
-//   hero: {
-//     name: "Hero",
-//     avatar: 0,
-//     health: 100,
-//     wins: 0,
-//     loses: 0,
-//     body: {
-//       head: {
-//         protected: false,
-//       },
-//       neck: {
-//         protected: false,
-//       },
-//       body: {
-//         protected: false,
-//       },
-//       belly: {
-//         protected: false,
-//       },
-//       legs: {
-//         protected: false,
-//       },
-//     },
-//     attackZones: ["head"],
-//     isCriticalAttack: false,
-//     damage: 10,
-//     critDamage: 30,
-//     isCritDamage: Math.random() * 10 < 2,
-//   },
-//   enemy: {
-//     name: "Enemy",
-//     avatar: 0,
-//     health: 100,
-//   },
-//   heroes: [{}],
-//   enemies: [{}],
-//   log: ["strs"],
-// };
+export const appState = {
+  hero: {
+    name: "",
+    avatar: 0,
+    health: 100,
+    wins: 0,
+    loses: 0,
+    body: {
+      head: {
+        protected: false,
+      },
+      neck: {
+        protected: false,
+      },
+      body: {
+        protected: false,
+      },
+      belly: {
+        protected: false,
+      },
+      legs: {
+        protected: false,
+      },
+    },
+    attackZones: ["head"],
+    isCriticalAttack: false,
+    damage: 10,
+    critDamage: 30,
+    isCritDamage: Math.random() * 10 < 2,
+  },
+  enemy: {
+    name: "Enemy",
+    avatar: 0,
+    health: 100,
+  },
+  heroes: [{}],
+  enemies: [{}],
+  log: ["strs"],
+};
